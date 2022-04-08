efd6ea6b7a5a7976297c008a1b1d2c99eb0176a4
Igor Detkin <dean94@bk.ru>
2022-04-08 11:26:52 +0600

    js/delete_comments

--- script.js
+++ script.js
@@ -32,26 +32,14 @@ let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                              // Так мы можем определить свою логику отправки.
-                                              // О том, как это делать, расскажем позже.
+  evt.preventDefault();
 
 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();
-
-  // Получите значение полей jobInput и nameInput из свойства value
-
-  // Выберите элементы, куда должны быть вставлены значения полей
-
-  // Вставьте новые значения с помощью textContent
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
